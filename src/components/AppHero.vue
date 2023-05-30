ef4347262f0a7c998fc2b27f69b5b5ed3bc93bf2
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-30 16:58:32 +0200

    esercizio completo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default (await import('vue')).defineComponent({
+  name: "AppTitle"
+})
+
+</script>
+
+<template>
+
+<img src="../assets/screenshot_000003.jpg" alt="knowhere" />
+
+  
+</template>
+
+<style scoped>
+img{
+    height: 500px;
+}
+img:hover
+{filter: drop-shadow(0 0 2em #646cffaa);}
+</style>
